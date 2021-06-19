5f69e273163b9ccac9edcba34a5882aec3abae30
Suyog-23 <suyog231002@gmail.com>
2021-06-19 10:33:06 +0530

    Some changes for local

--- searchengine/settings.py
+++ searchengine/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,4 +122,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
